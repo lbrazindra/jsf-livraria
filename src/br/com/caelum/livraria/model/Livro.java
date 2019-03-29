f289d39ca67e8be7429912d47f8c7c6eddccfeeb
Lucca <brazlucca@gmail.com>
2019-03-29 19:43:27 -0300

    segundo commmit

--- src/br/com/caelum/livraria/model/Livro.java
+++ src/br/com/caelum/livraria/model/Livro.java
@@ -1,12 +1,31 @@
 package br.com.caelum.livraria.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Livro {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
 	private String titulo;
 	private String isbn;
 	private double preco;
 	private String dataLancamento;
+	@ManyToMany
+	private List<Autor> autores = new ArrayList<Autor>();
 
 	public Livro() {
+	}
+
+	public void adicionaAutor(Autor autor) {
+		this.autores.add(autor);
 	}
 
 	public String getTitulo() {
@@ -39,5 +58,21 @@ public class Livro {
 
 	public void setDataLancamento(String dataLancamento) {
 		this.dataLancamento = dataLancamento;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public List<Autor> getAutores() {
+		return autores;
+	}
+
+	public void setAutores(List<Autor> autores) {
+		this.autores = autores;
 	}
 }
